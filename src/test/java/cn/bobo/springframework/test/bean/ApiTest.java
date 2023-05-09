bde51d26fd54c48522ff64cb86aa09b8faee17b1
bobo <2460417845@qq.com>
2023-05-09 16:10:34 +0800

    bo，feature:初始化和销毁方法

--- src/test/java/cn/bobo/springframework/test/bean/ApiTest.java
+++ src/test/java/cn/bobo/springframework/test/bean/ApiTest.java
@@ -1,19 +1,9 @@
 package cn.bobo.springframework.test.bean;
 
-import cn.bobo.springframework.beans.PropertyValue;
-import cn.bobo.springframework.beans.PropertyValues;
-import cn.bobo.springframework.beans.factory.config.BeanDefinition;
-import cn.bobo.springframework.beans.factory.config.BeanReference;
-import cn.bobo.springframework.beans.factory.supprt.DefaultListableBeanFactory;
-import cn.bobo.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import cn.bobo.springframework.context.ClassPathXmlApplicationContext;
 import cn.bobo.springframework.core.io.DefaultResourceLoader;
-import cn.bobo.springframework.test.bean.dao.UserDao;
 import cn.bobo.springframework.test.bean.service.UserService;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.IOException;
 
 /**
  * @author by bobo
@@ -23,57 +13,18 @@ import java.io.IOException;
 public class ApiTest {
     private DefaultResourceLoader resourceLoader;
 
-    @Before
-    public void init() {
-        resourceLoader = new DefaultResourceLoader();
-    }
-
-    @Test
-    public void test_BeanFactory() {
-        // 1.初始化 BeanFactory
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-
-        // 2. UserDao 注册
-        beanFactory.registerBeanDefinition("userDao", new BeanDefinition(UserDao.class));
-
-        // 3. UserService 设置属性[uId、userDao]
-        PropertyValues propertyValues = new PropertyValues();
-        propertyValues.addPropertyValue(new PropertyValue("uId", "10002"));
-        propertyValues.addPropertyValue(new PropertyValue("userDao", new BeanReference("userDao")));
-
-        // 4. UserService 注入bean
-        BeanDefinition beanDefinition = new BeanDefinition(UserService.class, propertyValues);
-        beanFactory.registerBeanDefinition("userService", beanDefinition);
-
-        // 5. UserService 获取bean
-        UserService userService = (UserService) beanFactory.getBean("userService");
-        userService.queryUserInfo();
-    }
-
-    @Test
-    public void test_withXmlResource() throws IOException {
-        // 1.初始化 BeanFactory
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-
-        // 2. 读取配置文件&注册Bean
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-        reader.loadBeanDefinitions("classpath:spring.xml");
-
-        // 3. 获取Bean对象调用方法
-        UserService userService = (UserService) beanFactory.getBean("userService", UserService.class);
-        String result = userService.queryUserInfo();
-        System.out.println("测试结果：" + result);
-    }
-
     @Test
     public void test_xml() {
         // 1.初始化 BeanFactory
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
         applicationContext.registerShutdownHook();
+
         // 2. 获取Bean对象调用方法
         UserService userService = applicationContext.getBean("userService", UserService.class);
         String result = userService.queryUserInfo();
         System.out.println("测试结果：" + result);
+        System.out.println("ApplicationContextAware：" + userService.getApplicationContext());
+        System.out.println("BeanFactoryAware：" + userService.getBeanFactory());
     }
 
 }
--- src/test/java/cn/bobo/springframework/test/bean/service/UserService.java
+++ src/test/java/cn/bobo/springframework/test/bean/service/UserService.java
@@ -1,7 +1,12 @@
 package cn.bobo.springframework.test.bean.service;
 
-import cn.bobo.springframework.beans.factory.DisposableBean;
-import cn.bobo.springframework.beans.factory.InitializingBean;
+import cn.bobo.springframework.beans.BeansException;
+import cn.bobo.springframework.beans.factory.BeanClassLoaderAware;
+import cn.bobo.springframework.beans.factory.BeanFactory;
+import cn.bobo.springframework.beans.factory.BeanFactoryAware;
+import cn.bobo.springframework.beans.factory.BeanNameAware;
+import cn.bobo.springframework.context.ApplicationContext;
+import cn.bobo.springframework.context.ApplicationContextAware;
 import cn.bobo.springframework.test.bean.dao.UserDao;
 
 /**
@@ -9,17 +14,43 @@ import cn.bobo.springframework.test.bean.dao.UserDao;
  * @Description 测试service
  * @Date 2023/4/22 21:51
  */
-public class UserService implements InitializingBean, DisposableBean {
+public class UserService implements BeanNameAware, BeanClassLoaderAware, ApplicationContextAware, BeanFactoryAware {
+
+    private ApplicationContext applicationContext;
+    private BeanFactory beanFactory;
 
     private String uId;
     private String company;
     private String location;
     private UserDao userDao;
 
-    public String queryUserInfo() {
-        return userDao.queryUserName(uId) + ", 公司：" + company + ", 地点" + location;
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean Name is：" + name);
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("ClassLoader：" + classLoader);
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
 
     public String getuId() {
         return uId;
@@ -53,13 +84,8 @@ public class UserService implements InitializingBean, DisposableBean {
         this.userDao = userDao;
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("执行userservice销毁");
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("service bean初始化完成");
+    public String queryUserInfo() {
+        return userDao.queryUserName(uId);
     }
 }
+
